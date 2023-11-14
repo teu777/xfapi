8731be2054384812895d9967683018a72cd3013e
Eugene Tarasov <eugene.tarasov@itrenew.com>
2023-11-14 20:09:59 +0100

    initialization

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+import os.path
+from pathlib import Path
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/<locale>/<fid>")
+def read_item(locale, fid):
+    fullpath = os.path.join(Path(__file__).parent, f"data/{locale}/{fid[0:2]}/{(fid)[2:]}.txt")
+    xftext = "N/A"
+    with open(fullpath, "r", encoding="utf-8") as ffile:
+       xftext = ffile.readlines();
+    return {"version": "pilot", "factid": fid, "locale": locale, "xfact": xftext}
