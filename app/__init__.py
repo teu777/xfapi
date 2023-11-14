4e11cf14c6f59e80d1acc7cfa95dcba39beda544
Eugene Tarasov <eugene.tarasov@itrenew.com>
2023-11-14 20:42:44 +0100

    initialization

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
+    return {"xf api"}
 
 @app.get("/items/<locale>/<fid>")
-def read_item(locale, fid):
+def read_item(locale: str, fid: str):
     fullpath = os.path.join(Path(__file__).parent, f"data/{locale}/{fid[0:2]}/{(fid)[2:]}.txt")
     xftext = "N/A"
     with open(fullpath, "r", encoding="utf-8") as ffile:
-       xftext = unicode(ffile.readlines()[0], 'utf-8');
+       xftext = ffile.readlines()[0]
     return {"version": "pilot", "factid": fid, "locale": locale, "xfact": xftext}
