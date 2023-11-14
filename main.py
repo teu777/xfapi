72f0bb44d6ee152662a9b4fecbc2d48a8654b0e7
Eugene Tarasov <eugene.tarasov@itrenew.com>
2023-11-14 19:54:59 +0100

    initialization

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os.path
 from pathlib import Path
 
-from typing import Union
-from fastapi import FastAPI
+from flask import Flask
 
-app = FastAPI()
+app = Flask(__name__)
 
 
 @app.get("/")
@@ -12,10 +11,10 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{locale}/{fid}")
-def read_item(locale: str, fid: str):
-    fullpath = os.path.join(Path(__file__).parent, f"data/{locale}/{fid[0:2]}/{(fid)[3:]}.txt")
+@app.get("/items/<locale>/<fid>")
+def read_item(locale, fid):
+    fullpath = os.path.join(Path(__file__).parent, f"data/{locale}/{fid[0:2]}/{(fid)[2:]}.txt")
     xftext = "N/A"
-    with open(fullpath, "r") as ffile:
+    with open(fullpath, "r", encoding="utf-8") as ffile:
        xftext = ffile.readlines();
     return {"version": "pilot", "factid": fid, "locale": locale, "xfact": xftext}
